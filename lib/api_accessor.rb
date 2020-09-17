41b4d2edc4bf2794be4be355452c1b93c41cc2b1
Kelin Field <kfield111@hotmail.com>
2020-09-17 03:22:53 +0000

    functioning list populator

--- lib/api_accessor.rb
+++ lib/api_accessor.rb
@@ -8,6 +8,25 @@ class API_ACCESSOR
   base_uri "data.ivanstanojevic.me"   #sets the base homepage url for the httparty gem
 
 
+  def get_sat_name_and_id     #a method that iterates through each page of the API and returns an array of extracted satellite ID's and names.
+    response = self.class.get('/api/tle/')    #the initial get query to the api endpoint.
+    list_body = JSON.parse(response.body)      #parses the response.
+
+
+    full_list = []      #creates a variable set to an empty array to be populated with satellite names by the while loop below.
+
+    while list_body["view"]["next"] != "https://data.ivanstanojevic.me/api/tle?page=10"   #creating a loop that runs unit the query reaches page 10 of the API.
+      next_page = list_body["view"]["next"]      #creates a variable to store the nested value of "next" within the variable assigned to the query response.
+      list_body["member"].each do |temp|      #iterates over the 20 (max) satellites in the current page stored in the "member" key.
+        full_list << temp['name']             #shovels the names of each satellite into the full_list array.
+        end
+      list = self.class.get(next_page)     #increments the url by resetting it to the "next" value we stored above.
+      list_body = JSON.parse(list.body)    #parses the new response.
+      puts "on page #{list_body["view"]["@id"]}, moving to #{list_body["view"]["next"]}"    #a development output that confirms the current page number and lists which page the loop is going to next.
+    end
+    puts full_list    #returns the collective names of all satellites shoveled into the array during the loop
+  end
+
 
   def search_api_for_sat
     api_url = "/api/tle/"      #sets a variable equal to the Endpoint.
@@ -53,9 +72,8 @@ class API_ACCESSOR
   end
 
 
-
 end
 
 
 first_test = API_ACCESSOR.new
-puts first_test.get_sat_data
+puts first_test.get_sat_name_and_id
