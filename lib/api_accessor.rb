fc4372624a58e008fac978be3742047dfb7475a9
Kelin Field <kfield111@hotmail.com>
2020-09-18 02:41:37 +0000

    Added next page method

--- lib/api_accessor.rb
+++ lib/api_accessor.rb
@@ -8,23 +8,36 @@ class API_ACCESSOR
   base_uri "data.ivanstanojevic.me"   #sets the base homepage url for the httparty gem
 
 
-  def get_sat_name_and_id     #a method that iterates through each page of the API and returns an array of extracted satellite ID's and names.
-    response = self.class.get('/api/tle/')    #the initial get query to the api endpoint.
-    list_body = JSON.parse(response.body)      #parses the response.
+def response
+  response = self.class.get('/api/tle/')    #the initial get query to the api endpoint.
+  @list_body = JSON.parse(response.body)      #parses the response and sets it equal to an instance variable.
+end
 
 
-    full_list = []      #creates a variable set to an empty array to be populated with satellite names by the while loop below.
+  @@full_list = []      #creates a variable set to an empty array to be populated with satellite names by the while loop below.
 
-    while list_body["view"]["next"] != "https://data.ivanstanojevic.me/api/tle?page=10"   #creating a loop that runs unit the query reaches page 10 of the API.
-      next_page = list_body["view"]["next"]      #creates a variable to store the nested value of "next" within the variable assigned to the query response.
-      list_body["member"].each do |temp|      #iterates over the 20 (max) satellites in the current page stored in the "member" key.
-        full_list << temp['name']             #shovels the names of each satellite into the full_list array.
-        end
-      list = self.class.get(next_page)     #increments the url by resetting it to the "next" value we stored above.
-      list_body = JSON.parse(list.body)    #parses the new response.
-      puts "on page #{list_body["view"]["@id"]}, moving to #{list_body["view"]["next"]}"    #a development output that confirms the current page number and lists which page the loop is going to next.
+  def get_sat_name_and_id (url)    #a method that iterates through each page of the API and returns an array of extracted satellite ID's and names.
+    current_page = @list_body["view"]["@id"].split("?")[1].tr("=", "")   #creates a variable and assigns it to the value of the current page and formats it to a more pleasing form.
+    @list_body["member"].each do |temp|      #iterates over the 20 (max) satellites in the current page stored in the "member" key.
+      @@full_list << temp['name']         #shovels the names of each satellite into the full_list array.
     end
-    puts full_list    #returns the collective names of all satellites shoveled into the array during the loop
+    puts "Currently displaying results from #{current_page}"  #indicates to the user which page of the API they are currently viewing.
+    @@full_list.each_with_index do |value, index|  #returns the collective names of all satellites shoveled into the array during the loop
+      puts "#{index+1}. #{value}"
+    end
+  end
+
+  def go_to_next_page     #a mpthod that clears the current list of satellites and then displays the satellites on page 2 of the API.
+    @@full_list.clear     #clears the current array of satellite names in the array.
+    next_page = @list_body["view"]["next"].split("?")[1]      #creates a variable to store the nested value of "next" within the variable assigned to the query response and formats it.
+
+    while @list_body["view"]["next"] == "https://data.ivanstanojevic.me/api/tle?#{next_page}"   #creating a loop that runs unit the query reaches a specific page number of the API pulled form the variable above.
+      next_page_address = @list_body["view"]["next"]  #sets a variable equal to the html address of the next page of the API.
+      list = self.class.get(next_page_address)     #increments the url by resetting it to the "next" value we stored above.
+      @list_body = JSON.parse(list.body)    #parses the new response.
+      puts "moving to #{next_page}"    #lists to the user which page the loop is going to next.
+    end
+    get_sat_name_and_id (next_page_address)   #calls the shown method and passes in the updated html address in order to show the new group of satellite names.
   end
 
 
@@ -45,12 +58,8 @@ class API_ACCESSOR
     sat_name = all_data["name"]
     ballistic_coef = all_data["line1"].split[4]
     inclination = all_data["line2"].split[1]
-
-
     designator = all_data["line1"].split[2]             #the following lines of code convert the data format from the api into a readable format to display which number this satellite launch was.
     number_launched = "#{designator[2]}#{designator[3]}#{designator[4]}"
-
-
     launch_date = all_data["line1"].split[2]              #the following lines of code convert the data format from the api into a readable year format.
     launch_year = "#{launch_date[0]}#{launch_date[1]}"
     if launch_date[0] == "9"
@@ -58,8 +67,6 @@ class API_ACCESSOR
     elsif launch_date[1] == "0"
       launch_year_full = "20#{launch_year}"
     end
-
-
 
     puts "Satellite ID: #{sat_id}"              #the following lines of code puts out the specified extracted data for the end user
     puts "Satellite Name: #{sat_name}"
@@ -71,9 +78,9 @@ class API_ACCESSOR
     # puts "Launch Date: #{all_data["date"]}"
   end
 
-
 end
 
 
 first_test = API_ACCESSOR.new
-puts first_test.get_sat_name_and_id
+first_test.response
+1.times {puts first_test.go_to_next_page}
